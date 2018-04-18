7bb601a17595bd4995f8acc9c6838e71d1be033f
Athellas <mm.krupinski@gmail.com>
2018-04-18 04:05:24 +0200

    start

--- FunWithObservableCollection/Program.cs
+++ FunWithObservableCollection/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace FunWithObservableCollection
 {
@@ -6,7 +8,43 @@ namespace FunWithObservableCollection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ObservableCollection<Person> people = new ObservableCollection<Person>()
+            {
+                new Person{ FirstName = "Peter", LastName = "Murphy", Age = 52 },
+                new Person{ FirstName = "Kevin", LastName = "Key", Age = 48 },
+            };
+
+            // wire up the CollectionChanged event
+            people.CollectionChanged += people_CollectionChanged;
+        }
+
+        static void people_CollectionChanged(object sender,
+            System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            // what was the action that caused the event?
+            Console.WriteLine("Action for this event: {0}", e.Action);
+
+            // they removed something
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+            {
+                Console.WriteLine("Here are the OLD items:");
+                foreach (Person p in e.OldItems)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+                Console.WriteLine();
+            }
+
+            // they added something
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                // Now show the NEW items that were inserted
+                Console.WriteLine("Here are the NEW items:");
+                foreach (Person p in e.NewItems)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+            }
         }
     }
 }
